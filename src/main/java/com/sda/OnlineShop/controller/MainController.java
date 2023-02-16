ee9b26adee2a51b83d926c994dc63a7719f3a49d
DESKTOP-PB2732I\gmaita00 <gmaita00@gmail.com>
2023-02-16 17:38:51 +0100

    implement add product logic

--- src/main/java/com/sda/OnlineShop/controller/MainController.java
+++ src/main/java/com/sda/OnlineShop/controller/MainController.java
@@ -1,22 +1,33 @@
 package com.sda.OnlineShop.controller;
 
 
+import com.sda.OnlineShop.dto.ProductDto;
+import com.sda.OnlineShop.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/addProduct")
-    public String addProductGet(){
+    public String addProductGet(Model model){
+        ProductDto productDto = new ProductDto();
+        model.addAttribute("productDto",productDto);
         //teoretic aici executam "bussines logic"
         //dupa care intoarcem un nume de pagina
         return "addProduct";
     }
-
     @PostMapping("/addProduct")
-    public String addProductPost(){
-        System.out.println("s-a apelat funtionalitatea add product");
+    public String addProductPost(@ModelAttribute ProductDto productDto){
+        productService.addProduct(productDto);
+
+        System.out.println(productDto);
         return "addProduct";
     }
 }
