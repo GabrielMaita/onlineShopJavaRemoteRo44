af161c1be84e3185289131b34aefb51d4ca7d3f4
DESKTOP-PB2732I\gmaita00 <gmaita00@gmail.com>
2023-02-23 18:41:36 +0200

    adaugat functionalitate email preexistent

--- src/main/java/com/sda/OnlineShop/controller/MainController.java
+++ src/main/java/com/sda/OnlineShop/controller/MainController.java
@@ -5,9 +5,11 @@ import com.sda.OnlineShop.dto.ProductDto;
 import com.sda.OnlineShop.dto.RegistrationDto;
 import com.sda.OnlineShop.services.ProductService;
 import com.sda.OnlineShop.services.RegistrationService;
+import com.sda.OnlineShop.validator.RegistrationDtoValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -18,12 +20,12 @@ import java.util.Optional;
 
 @Controller
 public class MainController {
-
-
     @Autowired
     private ProductService productService;
     @Autowired
     private RegistrationService registrationService;
+    @Autowired
+    private RegistrationDtoValidator registrationDtoValidator;
 
     @GetMapping("/addProduct")
     public String addProductGet(Model model) {
@@ -36,8 +38,7 @@ public class MainController {
 
     @PostMapping("/addProduct")
     public String addProductPost(@ModelAttribute ProductDto productDto,
-                                 @RequestParam("productImage") MultipartFile productImage)
-    {
+                                 @RequestParam("productImage") MultipartFile productImage) {
 
         productService.addProduct(productDto, productImage);
         System.out.println("S-a apelat functionalitatea de addProductPost");
@@ -55,8 +56,7 @@ public class MainController {
     @GetMapping("/product/{name}/{productId}")
     public String viewProductGet(Model model,
                                  @PathVariable(value = "productId") String productId,
-                                 @PathVariable(value = "name") String name)
-    {
+                                 @PathVariable(value = "name") String name) {
         Optional<ProductDto> optionalProductDto = productService.getOptionalProductDtoById(productId);
         if (optionalProductDto.isEmpty()) {
             return "error";
@@ -67,15 +67,18 @@ public class MainController {
     }
 
     @GetMapping("/registration")
-    public String viewRegistrationGet (Model model) {
+    public String viewRegistrationGet(Model model) {
         RegistrationDto registrationDto = new RegistrationDto();
         model.addAttribute("registrationDto", registrationDto);
         return "registration";
     }
 
     @PostMapping("/registration")
-    public String viewRegistrationPost(@ModelAttribute RegistrationDto registrationDto) {
-        System.out.println("S-a apelat functionalitatea reggistration " + registrationDto);
+    public String viewRegistrationPost(@ModelAttribute RegistrationDto registrationDto, BindingResult bindingResult) {
+        registrationDtoValidator.validate(registrationDto, bindingResult);
+        if (bindingResult.hasErrors()){
+            return "registration";
+        }
         registrationService.addRegistration(registrationDto);
         return "registration";
     }
